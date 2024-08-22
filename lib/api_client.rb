3eff8431e40839fb4696ce5b4336ce2501b861ef
Alvaro Véliz <alvaro.veliz@gmail.com>
2024-08-22 17:58:44 -0400

    Refactoring to a cleaner version

--- lib/api_client.rb
+++ lib/api_client.rb
@@ -7,15 +7,27 @@ class ApiClient
   BASE_URL = 'https://chita.cl/api/v1/pricing/simple_quote'
 
   def fetch_quote(invoice)
-    uri = URI("#{BASE_URL}?client_dni=#{invoice.emitter_rut}&debtor_dni=#{invoice.debtor_rut}&document_amount=#{invoice.amount}&folio=#{invoice.folio}&expiration_date=#{invoice.due_date}")
+    uri = build_uri(invoice)
+    response = make_request(uri)
+    parse_response(response)
+  end
 
+  private
+
+  def build_uri(invoice)
+    URI("#{BASE_URL}?client_dni=#{invoice.emitter_rut}&debtor_dni=#{invoice.debtor_rut}&document_amount=#{invoice.amount}&folio=#{invoice.folio}&expiration_date=#{invoice.due_date}")
+  end
+
+  def make_request(uri)
     request = Net::HTTP::Get.new(uri)
     request['X-Api-Key'] = API_KEY
 
-    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
       http.request(request)
     end
-    
+  end
+
+  def parse_response(response)
     JSON.parse(response.body)
   end
 end
